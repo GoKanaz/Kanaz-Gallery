e44abb9e0fe9c22bd4e71f03aa7e6206ee5b6869
GoKanaz <gokanaz.official@gmail.com>
2026-02-17 18:28:43 +0700

    fix: use NavHostFragment to get NavController in MainActivity

--- app/src/main/java/com/gokanaz/gallery/activities/MainActivity.kt
+++ app/src/main/java/com/gokanaz/gallery/activities/MainActivity.kt
@@ -2,24 +2,26 @@ package com.gokanaz.gallery.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.gokanaz.gallery.R
 import com.gokanaz.gallery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    
+
     private lateinit var binding: ActivityMainBinding
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
         val navView: BottomNavigationView = binding.bottomNavigation
-        val navController = findNavController(R.id.nav_host_fragment)
-        
         navView.setupWithNavController(navController)
     }
 }
